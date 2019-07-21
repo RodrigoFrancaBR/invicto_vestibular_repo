9b109b3ad09314fee30ff1e988698e26e43d97fe
RodrigoFrancaBR <rfrancacosta@gmail.com>
2019-07-21 13:28:09 -0300

    Iniciando com JPA, criação da entidade Usuario.

---
+++ invictoweb/src/main/java/CriarTabelas.java
@@ -0,0 +1,9 @@
+import javax.persistence.Persistence;
+
+public class CriarTabelas {
+
+	public static void main(String[] args) {
+		 Persistence.createEntityManagerFactory("invictoPU");		
+	}
+
+}
---
+++ invictoweb/src/main/java/invictoweb/model/Usuario.java
@@ -0,0 +1,78 @@
+package invictoweb.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario implements Serializable{	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8002195891595593279L;	
+	private Long id;
+	private String email;
+	private String senha;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Id
+	@GeneratedValue
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", email=" + email + ", senha=" + senha + "]";
+	}
+
+}
